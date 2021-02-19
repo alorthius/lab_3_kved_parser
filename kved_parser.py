847ff1dcbdc7b4d15dd9a03669144a6e7c3e863a
Alina Bondarets <alinium3012@gmail.com>
2021-02-19 22:54:23 +0200

    Add main func

--- kved_parser.py
+++ kved_parser.py
@@ -1,17 +1,16 @@
 import json
 from pprint import pprint
+from collections import OrderedDict
+
 
 def read_file(path: str) -> dict:
     with open('kved.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
-kved_dict = read_file('path')
-# pprint(kved_dict)
-
 
 def create_subdict(name: str, type_value: str, num_children: int, parent: str) -> dict:
-    subdict = {}
+    subdict = OrderedDict()
     subdict['name'] = name
     subdict['type'] = type_value
     if num_children:
@@ -31,28 +30,41 @@ def create_dictionary(kved_dict: dict, class_code: str):
     for section_index in range(len(all_sections)):
         section_code = all_sections[section_index]['sectionCode']
         num_children = len(all_sections[section_index]['divisions'])
-        section_dict = create_subdict(section_code, 'section', num_children, None)
+        section_dict = create_subdict(
+            section_code, 'section', num_children, None)
 
         for division in range(len(all_sections[section_index]['divisions'])):
             if division_code != all_sections[section_index]['divisions'][division]['divisionCode']:
                 continue
             division_name = all_sections[section_index]['divisions'][division]['divisionName']
-            num_children = len(all_sections[section_index]['divisions'][division]['groups'])
-            division_dict = create_subdict(division_name, 'division', num_children, section_dict)
+            num_children = len(
+                all_sections[section_index]['divisions'][division]['groups'])
+            division_dict = create_subdict(
+                division_name, 'division', num_children, section_dict)
 
             for group in all_sections[section_index]['divisions'][division]['groups']:
                 if group_code != group['groupCode']:
                     continue
                 group_name = group['groupName']
                 num_children = len(group['classes'])
-                group_dict = create_subdict(group_name, 'group', num_children, division_dict)
+                group_dict = create_subdict(
+                    group_name, 'group', num_children, division_dict)
 
                 for class_dict in group['classes']:
                     if class_code == class_dict['classCode']:
                         class_name = class_dict['className']
-                        final_dict = create_subdict(class_name, 'class', None, group_dict)
+                        final_dict = create_subdict(
+                            class_name, 'class', None, group_dict)
 
     return final_dict
 
-class_code = "01.11"
-pprint(create_dictionary(kved_dict, class_code))
+
+def write_file(kved_dict: dict):
+    with open('kved_results.json', 'w', encoding='UTF-8') as file:
+        json.dump(kved_dict, file, indent=4, ensure_ascii=False)
+
+
+def parse_kved(class_code: str):
+    kved_dict = read_file('kved.json')
+    final_dict = create_dictionary(kved_dict, class_code)
+    write_file(final_dict)
